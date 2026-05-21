45a1366021f3a41ced45677187d5378322fa1e69
Polina <123462814+Domenka@users.noreply.github.com>
2026-05-21 18:38:42 +0400

    added cruds method for booking

--- core/clients/api_client.py
+++ core/clients/api_client.py
@@ -3,15 +3,18 @@ from http.client import responses
 import requests
 import os
 from dotenv import load_dotenv
+from requests.auth import HTTPBasicAuth
+
 from core.settings.environments import Environment
 import allure
 from core.clients.endpoints import Endpoints
 from core.settings.config import Creds, Timeouts
-from core.schemas.booking_schema import BOOKING_SCHEMA
+from core.schemas.booking_schema import BOOKING_SCHEMA, BOOKINGIDS_SCHEMA
+
 import jsonschema
 
-
 load_dotenv()
+
 
 class ApiClient():
     def __init__(self):
@@ -28,7 +31,6 @@ class ApiClient():
             "Content-Type": "application/json"
         }
 
-
     def get_base_url(self, environment: Environment) -> str:
         if environment == Environment.TEST:
             return os.getenv('TEST_BASE_URL')
@@ -39,7 +41,6 @@ class ApiClient():
         else:
             raise ValueError(f"Unsupported environment: {environment}")
 
-
     def get(self, endpoint, params=None, status_code=200):
         url = self.base_url + endpoint
         response = requests.get(url, headers=self.headers, params=params)
@@ -47,14 +48,12 @@ class ApiClient():
             assert response.status_code == status_code
         return response.json()
 
-
     def post(self, endpoint, data=None, status_code=200):
         url = self.base_url + endpoint
         response = requests.post(url, headers=self.headers, json=data)
         if response.status_code:
             assert response.status_code == status_code
         return response.json()
-
 
     def ping(self):
         with allure.step("Ping api client"):
@@ -64,9 +63,7 @@ class ApiClient():
 
         with allure.step("Assert status code"):
             assert response.status_code == 201, f"Expected status code is 201, but got {response.status_code}"
-
         return response.status_code
-
 
     def auth(self):
         with allure.step("Getting auth"):
@@ -78,10 +75,8 @@ class ApiClient():
         with allure.step("Assert status code"):
             assert response.status_code == 200, f"Expected status code is 200, but got {response.status_code}"
         token = response.json().get("token")
-
         with allure.step("Updating header with auth"):
             self.session.headers.update({"Authorization": f"Bearer {token}"})
-
 
     def get_booking_by_id(self, booking_id):
         with allure.step("Get booking by id"):
@@ -95,6 +90,61 @@ class ApiClient():
         with allure.step("Assert format and content from response"):
             assert isinstance(response.json(), dict)
             jsonschema.validate(response.json(), BOOKING_SCHEMA)
-
             return response.json()
 
+    def delete_booking_by_id(self, booking_id):
+        with allure.step("Delete booking by id"):
+            url = f"{self.base_url}{Endpoints.BOOKING_ENDPOINT}/{booking_id}"
+            response = self.session.delete(url, auth=HTTPBasicAuth(Creds.USERNAME, Creds.PASSWORD))
+            response.raise_for_status()
+        with allure.step("Assert status code"):
+            assert response.status_code == 201, f"Expected status code is 201, but got {response.status_code}"
+            return response.status_code == 201
+
+    def create_booking(self, booking_data):
+        with allure.step("Create booking"):
+            url = f"{self.base_url}{Endpoints.BOOKING_ENDPOINT}"
+            response = self.session.post(url, json=booking_data)
+            response.raise_for_status()
+            with allure.step("Assert status code"):
+                assert response.status_code == 200, f"Expected status code is 200, but got {response.status_code}"
+                with allure.step("Assert format and content from response"):
+                    assert isinstance(response.json(), dict)
+                    jsonschema.validate(response.json(), BOOKING_SCHEMA)
+            return response.json()
+
+    def get_booking_ids(self, params=None):
+        with allure.step("Getting object with booking"):
+            url = f"{self.base_url}{Endpoints.BOOKING_ENDPOINT}"
+            response = self.session.get(url, params=params)
+            response.raise_for_status()
+            with allure.step("Assert status code"):
+                assert response.status_code == 200, f"Expected status code is 200, but got {response.status_code}"
+                with allure.step("Assert format and content from response"):
+                    assert isinstance(response.json(), list)
+                    jsonschema.validate(response.json(), BOOKINGIDS_SCHEMA)
+                    return response.json()
+
+    def update_booking(self, booking_id, booking_data):
+        with allure.step("Update booking by id"):
+            url = f"{self.base_url}{Endpoints.BOOKING_ENDPOINT}/{booking_id}"
+            response = self.session.put(url, json=booking_data, auth=HTTPBasicAuth(Creds.USERNAME, Creds.PASSWORD))
+            response.raise_for_status()
+            with allure.step("Assert status code"):
+                assert response.status_code == 200, f"Expected status code is 200, but got {response.status_code}"
+                with allure.step("Assert format and content from response"):
+                    assert isinstance(response.json(), dict)
+                    jsonschema.validate(response.json(), BOOKING_SCHEMA)
+            return response.json()
+
+    def partial_update_booking(self, booking_id, booking_data):
+        with allure.step("Partial update booking by id"):
+            url = f"{self.base_url}{Endpoints.BOOKING_ENDPOINT}/{booking_id}"
+            response = self.session.put(url, json=booking_data, auth=HTTPBasicAuth(Creds.USERNAME, Creds.PASSWORD)) #удивительно, но в документации действительно указан put-метод
+            response.raise_for_status()
+            with allure.step("Assert status code"):
+                assert response.status_code == 200, f"Expected status code is 200, but got {response.status_code}"
+                with allure.step("Assert format and content from response"):
+                    assert isinstance(response.json(), dict)
+                    jsonschema.validate(response.json(), BOOKING_SCHEMA)
+            return response.json()
--- core/schemas/booking_schema.py
+++ core/schemas/booking_schema.py
@@ -19,3 +19,17 @@ BOOKING_SCHEMA = {
         "bookingdates"
     ],
 }
+
+BOOKINGIDS_SCHEMA = {
+    "type": "array",
+    "items": {
+        "type": "object",
+        "properties": {
+            "bookingid": {
+                "type": "integer"
+            }
+        },
+        "required": ["bookingid"],
+        "additionalProperties": False
+    }
+}
